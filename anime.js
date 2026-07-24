ce258ac99461668dcd74f48980724942e3e5ea40
Anonym Arch User <75336897532579754357a@gmail.com>
2026-07-24 04:21:09 +0200

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,33 +1,94 @@
 const animeList = [
 
-{
-name:"One Piece",
-image:"https://placehold.co/300x400",
-link:"anime.html"
-},
+    {
+        name: "Overlord",
 
-{
-name:"Naruto",
-image:"https://placehold.co/300x400",
-link:"anime.html"
-},
+        cover: "https://imgs.search.brave.com/7QZZRYr4ieePV1-jrhrwk4dpwvmO7npY1zXHNfu8V94/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRpYS5hbmltYXRpb25kaWdpdGFsbmV0d29yay5jb20vaW1hZ2VzL3Nob3cvMmY2MTFkNDAtMjYwMi00MDkxLTg5Y2MtZGI2ZTMwY2YxZTk0L3BvcnRyYWl0LXdpdGgtbG9nby53aWR0aD01MDAscXVhbGl0eT05MA",
 
-{
-name:"Attack on Titan",
-image:"https://placehold.co/300x400",
-link:"anime.html"
-},
+        description:
+        "Overlord erzählt die Geschichte von Momonga, einem mächtigen Spieler, der nach dem Ende eines Online-Spiels in der Welt von Yggdrasil zurückbleibt und als mächtiger Magierherrscher Ainz Ooal Gown eine neue Welt erkundet.",
 
-{
-name:"Demon Slayer",
-image:"https://placehold.co/300x400",
-link:"anime.html"
-},
 
-{
-name:"Jujutsu Kaisen",
-image:"https://placehold.co/300x400",
-link:"anime.html"
-}
+        seasons: [
+
+            {
+                number: 1,
+
+
+                episodes: [
+
+                    {
+                        number: 1,
+                        embed: "https://voe.sx/e/ka201qh5q3rn"
+                    },
+
+                    {
+                        number: 2,
+                        embed: "https://voe.sx/e/56mmjdn32xvx"
+                    },
+
+                    {
+                        number: 3,
+                        embed: "https://voe.sx/e/r7cdgkq93m5g"
+                    },
+
+                    {
+                        number: 4,
+                        embed: "https://voe.sx/e/yzpx1apa027r"
+                    },
+
+                    {
+                        number: 5,
+                        embed: "https://voe.sx/e/gopr42fya5au"
+                    },
+
+                    {
+                        number: 6,
+                        embed: "https://voe.sx/e/cfvldxx5ogk3"
+                    },
+
+                    {
+                        number: 7,
+                        embed: "https://voe.sx/e/ev7xaa4fy9ct"
+                    },
+
+                    {
+                        number: 8,
+                        embed: "https://voe.sx/e/h7ju6owbjfpw"
+                    },
+
+                    {
+                        number: 9,
+                        embed: "https://voe.sx/e/0yu3uegbti9e"
+                    },
+
+                    {
+                        number: 10,
+                        embed: "https://voe.sx/e/me4gwyo90nhc"
+                    },
+
+                    {
+                        number: 11,
+                        embed: "https://voe.sx/e/lewpc4unajxd"
+                    },
+
+                    {
+                        number: 12,
+                        embed: "https://voe.sx/e/hc4hf2xltuoh"
+                    },
+
+                    {
+                        number: 13,
+                        embed: "",
+                        unavailable: true
+                    }
+
+                ]
+
+            }
+
+        ]
+
+    }
 
 ];
