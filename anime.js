da03aca338aaff9b988edb32f265c5d4fc7e1ded
Anonym Arch User <75336897532579754357a@gmail.com>
2026-07-24 03:13:57 +0200

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,1 +1,33 @@
+const animeList = [
 
+{
+name:"One Piece",
+image:"https://placehold.co/300x400",
+link:"anime.html"
+},
+
+{
+name:"Naruto",
+image:"https://placehold.co/300x400",
+link:"anime.html"
+},
+
+{
+name:"Attack on Titan",
+image:"https://placehold.co/300x400",
+link:"anime.html"
+},
+
+{
+name:"Demon Slayer",
+image:"https://placehold.co/300x400",
+link:"anime.html"
+},
+
+{
+name:"Jujutsu Kaisen",
+image:"https://placehold.co/300x400",
+link:"anime.html"
+}
+
+];
