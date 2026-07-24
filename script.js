e57199dfa398c9b33c0c7f583205acfa4ce9e1fc
Anonym Arch User <75336897532579754357a@gmail.com>
2026-07-24 03:14:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
+let shown = 0;
 
+const grid=document.getElementById("animeGrid");
+
+function loadAnime(){
+
+let amount=15;
+
+
+animeList.slice(shown,shown+amount).forEach(anime=>{
+
+
+let card=document.createElement("div");
+
+card.className="card";
+
+
+card.innerHTML=`
+
+<img src="${anime.image}">
+
+<h3>${anime.name}</h3>
+
+`;
+
+
+card.onclick=()=>{
+
+window.location.href=anime.link;
+
+};
+
+
+grid.appendChild(card);
+
+
+});
+
+
+shown+=amount;
+
+}
+
+
+
+loadAnime();
+
+
+
+document.getElementById("more").onclick=()=>{
+
+loadAnime();
+
+};
+
+
+
+document.getElementById("search").oninput=function(){
+
+
+let value=this.value.toLowerCase();
+
+
+document.querySelectorAll(".card").forEach(card=>{
+
+
+card.style.display=
+
+card.innerText.toLowerCase().includes(value)
+
+?"block":"none";
+
+
+});
+
+
+};
